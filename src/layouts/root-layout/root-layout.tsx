01013c5fc95692d81da88cf5499344fa1fa23f45
sunyang-629 <sunyang9876@hotmail.com>
2023-09-17 21:45:11 +1000

    5-lists & list icon

--- src/layouts/root-layout/root-layout.tsx
+++ src/layouts/root-layout/root-layout.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../../components";
 import { Grid, GridItem } from "@chakra-ui/react";
+import Sidebar from "../../components/sidebar/sidebar";
 
 const RootLayout: FC = () => {
   return (
@@ -13,7 +14,7 @@ const RootLayout: FC = () => {
         minHeight={{ lg: "100vh" }}
         p="30px"
       >
-        <span>sidebar</span>
+        <Sidebar />
       </GridItem>
       <GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }} p="40px">
         <Navbar />
